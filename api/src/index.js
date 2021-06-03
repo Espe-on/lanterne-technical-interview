79583121b8eeaae94945ebd1fe48ee676a79764f
espe-on <contact@josephcrawley.dev>
2021-06-03 20:03:50 +0100

    updated project to not use babel as was not working

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const express = require ('express')
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('hello world')
+});
+
+app.listen(3000, () =>
+    console.log('listening on port 3000')
+);
